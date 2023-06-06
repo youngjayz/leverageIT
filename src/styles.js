26813e7dd2a047cbba3f50d8712ed0ec2b543e80
Teem1345 <awogbuyitimothy@gmail.com>
2023-06-06 22:17:37 +0100

    done the trade dropdowns

--- src/styles.js
+++ src/styles.js
@@ -7,7 +7,14 @@ const styles = {
   container: "w-[86%] max-w-[86%] mx-auto ",
   container1: "w-[96%] max-w-[96%] mx-auto ",
   tradeCoin: "flex-1 text-sm 2xl:text-base text-gray-400 font-medium font-DM",
-  tradeCoinHi:"flex-1 text-sm 2xl:text-base text-gray-200 font-medium font-DM",
+  tradeCoinHi: "flex-1 text-sm 2xl:text-base text-gray-200 font-medium font-DM",
+
+  longBtn:
+    "transition duration-200 bg-buttongreen cursor-pointer rounded-full w-[48%] text-xs py-1 text-medium",
+  notLongBtn:
+    "transition duration-200 text-gray-400  cursor-pointer rounded-full w-[48%] text-xs py-1 text-medium",
+  isNotFills:"bg-gray-300 cursor-pointer whitespace-nowrap rounded-full text-xs font-medium text-sidebar px-4 py-1 transition duration-200",
+  isFills:"text-gray-400 cursor-pointer whitespace-nowrap rounded-full text-xs px-2 py-1 text-medium transition duration-200",
 };
 
 export {styles}
